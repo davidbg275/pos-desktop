93cc84a224aec225ce77e43d85c163f958716f5d
davidbg275 <dbibianog@gmail.com>
2025-08-31 16:51:36 -0600

    Agregando README.md y limpieza de .gitignore + cambios en mainApp

---
+++ README.md
@@ -0,0 +1,14 @@
+
+# POS Desktop (JavaFX)
+
+Aplicación POS para papelería: versión **desktop** en JavaFX (y más adelante Android).
+Este repo sigue buenas prácticas: Maven, JavaFX, FXML + Controller, y Git/GitHub.
+
+## Requisitos
+- JDK 24
+- Maven (o `mvnw` incluido en el repo)
+
+## Cómo ejecutar (dev)
+```bash
+mvn javafx:run
+
--- src/main/java/com/miempresa/pos/MainApp.java
+++ src/main/java/com/miempresa/pos/MainApp.java
@@ -1,26 +1,23 @@
 package com.miempresa.pos;
-
 import javafx.application.Application;
 
 public class MainApp extends Application {
     @Override
     public void start(javafx.stage.Stage stage) throws java.io.IOException {
+
         // Aquí diremos qué debe mostrarse en la ventana
         javafx.fxml.FXMLLoader loader = new javafx.fxml.FXMLLoader(
                 getClass().getResource("/ui/main.fxml")
         );
         javafx.scene.Scene scene = new javafx.scene.Scene(loader.load(),900, 600);
-
-        stage.setTitle("POS Desktop");
+        stage.setTitle("-----POS Desktop------");
         stage.setScene(scene);
         stage.show();
 
-
     }
+
     public static void main(String[] args){
         launch();
     }
-
-
 
 }
